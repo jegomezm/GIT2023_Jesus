8e100f61ee3aedd7fb15ea32e43b95d5e6a336b0
Jesús <a_jesus.gomez.marquez@iespablopicasso.es>
2023-02-03 13:15:04 +0100

    Primera version del archivo

---
+++ Vehiculo.java
@@ -0,0 +1,14 @@
+
+public class Vehiculo {
+  
+  private String marca;
+  private String modelo;
+  private String matricula;
+  
+  Vehiculo (String marca, String modelo, String matricula) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.matricula = matricula;
+  }
+}
+
